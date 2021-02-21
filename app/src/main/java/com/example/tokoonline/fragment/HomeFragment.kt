9740e1b15eb223a9fd41b710be55dd4a2bf6d010
slametaldiansyah <slametaldiansyah96@gmail.com>
2021-02-21 14:58:47 -0800

    add home page

--- app/src/main/java/com/example/tokoonline/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/tokoonline/fragment/HomeFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager.widget.ViewPager
 import com.example.tokoonline.R
+import com.example.tokoonline.adapter.AdapterSlider
 
 /**
  * A simple [Fragment] subclass.
@@ -13,11 +15,26 @@ import com.example.tokoonline.R
  */
 class HomeFragment : Fragment() {
 
+    lateinit var vpslider: ViewPager
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
+
+        //panggil id diview xml
+        vpslider = view.findViewById(R.id.vp_slider)
+
+        val arrSlider = ArrayList<Int>()
+        arrSlider.add(R.drawable.slider1)
+        arrSlider.add(R.drawable.slider2)
+        arrSlider.add(R.drawable.slider3)
+
+        val adapterSlider = AdapterSlider(arrSlider, activity)
+        vpslider.adapter = adapterSlider
+
+        return view
     }
 }
